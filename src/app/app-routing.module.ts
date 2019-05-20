e90a653817a1fed5711c4854e545b98c3ea1d352
Chris FFRR <chris@shotatdawn.net>
2019-05-20 19:31:04 +0200

    ionic 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import {  RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'interesser', loadChildren: './pages/interesser/interesser.module#InteresserPageModule' },
-  { path: 'chat', loadChildren: './pages/chat/chat.module#ChatPageModule' },
-  { path: 'loading', loadChildren: './pages/loading/loading.module#LoadingPageModule' },
-  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'interesser', loadChildren: './interesser/interesser.module#InteresserPageModule' },
+  { path: 'chat', loadChildren: './chat/chat.module#ChatPageModule' },
+  { path: 'loading', loadChildren: './loading/loading.module#LoadingPageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
 ];
 
 @NgModule({
