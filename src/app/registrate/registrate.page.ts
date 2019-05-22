9f48c1a309349728ec8696e8e8dcbfcb1c429afe
MikaelSvanasbakken <31248055+MikaelSvanasbakken@users.noreply.github.com>
2019-05-22 20:25:41 +0200

    Added register stuff
    added register page

--- src/app/registrate/registrate.page.ts
+++ src/app/registrate/registrate.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-registrate',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrate.page.scss'],
 })
 export class RegistratePage implements OnInit {
+  public statusMessage: string = "";
+  displayError: boolean = false;
+  public loginFormGroup: FormGroup;
 
-  constructor() { }
+  name: String = "test";
+  lastName: String = "test";
+  password: String = "test";
+  email: String = "test";
+  Institutt: String = "test";
+  Studie: String = "test";
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
+  register(form){
+    fetch('https://smidigprosjekt.azurewebsites.net/RegisterUser', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: this.name,
+        lastName: this.lastName,
+        password: this.password,
+        email: this.email,
+        Institutt: this.Institutt,
+        Studie: this.Studie
+      })
+    })
+}
 
 }
+
+
+
+
+
+
