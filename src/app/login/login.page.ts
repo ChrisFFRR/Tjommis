3eeba9d37fccd3903d3f045a55ca6d358a08d972
Chris FFRR <chris@shotatdawn.net>
2019-05-23 11:55:49 +0200

    styled login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {TjommisHubService} from "../services/tjommis-hub.service";
 import {AuthServiceService} from "../services/auth-service.service";
 import {Router} from "@angular/router";
