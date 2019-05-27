a2d68f4324753b0f5debb4847be0939951fa8adf
Mikael <msvanaas@gmail.com>
2019-05-27 12:17:10 +0200

    Rows
    Rows

--- src/app/interesser/interesser.page.ts
+++ src/app/interesser/interesser.page.ts
@@ -18,40 +18,40 @@ export class InteresserPage implements OnInit {
   public selectedTags: any[] = [];
 
 
-  skole = ["#eksamen", "#kollokvie", "#bachelor","#master", "#brukerundersøkelse", "#fadderuke"];
-  teknologi = ["#ps4", "#xbox", "#switch", "#mobil","#hardware", "#software","#mac","#windows", "#data"];
-  idrett = ["#fotball", "#basketball", "#extremsport", "håndball","#ski","#BMX","#Skateboard",];
-  kultur = ["#teater", "#kino", "#film", "#festival", "#litteratur", "#kunst", "#musikk"];
-  jobb = ["#jobbsøknad", "#internship", "#lønn", "#personutvikling","#deltidsjobb", "#opplæring"];
-  matdrikke = ["#resturant", "#øl", "#vin", "#sprit", "#julebord", "#kjøtt", "#fisk", "#bacon"];
-
-
   constructor(public router: Router) {
       this.getData()
   }
   getData()
   {
         this.items = [
-            {
-
-                "title": "Skole",
-                 "tag": ["#eksamen", "#kollokvie", "#bachelor","#master", "#brukerundersøkelse", "#fadderuke"]
-
+            {"title": "Gaming",
+                 "tag": ["#PS4", "#XBOX", "#PC","#LoL", "#Dota2", "#WoW"]
             },
-            {
-
-                "title": "Idrett",
-                "tag":["#fotball", "#basketball", "#extremsport", "håndball","#ski","#BMX","#Skateboard",]
+            {"title": "Skole",
+                "tag":["#Kollokvie", "#Eksamen", "#Tips", "Events","#Skolebøker",]
             },
-            {
-
-                "title": "Fest",
-                "tag": ["#teater", "#kino", "#film", "#festival", "#litteratur", "#kunst", "#musikk"]
+            {"title": "Idrett",
+                "tag": ["#Fotball", "#Vektløfting", "#Løping", "#Speedwalking", "#Basketball"]
             },
-            {
-              "title": "Kollokvie",
-                "tag": ["#jobbsøknad", "#internship", "#lønn", "#personutvikling","#deltidsjobb", "#opplæring"]
-            }]
+            {"title": "Teknologi",
+                "tag": ["#Programmering", "#Windows", "#Linux", "#IT","#Java", "#Javascript"]
+            },
+            {"title": "Mat",
+                "tag": ["#Schnitzler", "#Pizza", "#Kebab", "#Øl","#Sprit", "#Vin"]
+            } ,
+            {"title": "Musikk",
+                "tag": ["#Pop", "#Rock", "#Rap", "#Metall","#Techno", "#RnB", "Vapourwave"]
+            },
+            {"title": "Litteratur",
+                "tag": ["#Alf Prøysen", "#J.K Rowling", "#R.R Tolkien", "#Dan Abnett"]
+            },
+            {"title": "Jobb",
+                "tag": ["#CV", "#Internship", "#Jobbsøknad", "#Konferanser", "#Workshops"]
+            },
+            {"title": "Fest",
+                "tag": ["#Vorspiel", "#Nachspiel", "#Hjemmefest", "#Låvefest", "#Alkoholfritt"]
+            },
+            ]
   }
 
   btnActivate(ionicButton) {
