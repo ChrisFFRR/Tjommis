8dd62fc9857388f9c9a49767712c1082691e0110
Chris FFRR <chris@shotatdawn.net>
2019-05-27 14:37:13 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {  RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  { path: '', redirectTo: 'interesser', pathMatch: 'full' },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
 
 
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
