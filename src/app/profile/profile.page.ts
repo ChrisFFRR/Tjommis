5d55ce7e6c31be2c5c644cf80928a29940f86e00
Chris FFRR <chris@shotatdawn.net>
2019-06-04 16:14:35 +0200

    showing lobbies in profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,87 +1,90 @@
 import {Component, NgZone, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Events} from "@ionic/angular";
-import {TjommisHubService} from "../services/tjommis-hub.service";
+import {Lobby, TjommisHubService} from "../services/tjommis-hub.service";
 import anime from 'animejs';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import {connectableObservableDescriptor} from 'rxjs/internal/observable/ConnectableObservable';
 
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+    selector: 'app-profile',
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
 
-  constructor(
-      public router: Router,
-      public tjommisHub: TjommisHubService,
-      public events: Events,
-      private zone: NgZone) {
+    constructor(
+        public router: Router,
+        public tjommisHub: TjommisHubService,
+        public events: Events,
+        private zone: NgZone) {
 
-    this.events.subscribe('connectedusers', (data) => {
-      this.onUpdateConnectedUsers(data);
-    });
-    this.events.subscribe('username', (data) => {
-      console.log("Profile.OnUpdateUserName",data);
-      this.onUpdateUsername(data);
-    });
-  }
+        this.events.subscribe('connectedusers', (data) => {
+            this.onUpdateConnectedUsers(data);
 
-  username: string = this.tjommisHub.connectionInfo ? this.tjommisHub.connectionInfo.userInfo.username : null;
-  connectedUsers: number = 0;
+        });
+        this.events.subscribe('username', (data) => {
+            console.log("Profile.OnUpdateUserName", data);
+            this.onUpdateUsername(data);
+        });
+    }
+
+    lobbies: Lobby[] = this.tjommisHub.rooms ? this.tjommisHub.rooms : [];
+    username: string = this.tjommisHub.connectionInfo ? this.tjommisHub.connectionInfo.userInfo.username : null;
+    connectedUsers: number = 0;
 
 
-  onUpdateConnectedUsers = number => {
-    this.zone.run(() => {
-      this.connectedUsers = number;
-      console.log("connected users: ", number)
-    });
-  };
+    onUpdateConnectedUsers = number => {
+        this.zone.run(() => {
+            this.connectedUsers = number;
+            console.log("connected users: ", number)
+        });
+    };
 
-  onUpdateUsername = username => {
-    this.zone.run(() => {
-      this.username = username;
-    });
-  };
+    onUpdateUsername = username => {
+        this.zone.run(() => {
+            this.username = username;
+        });
+    };
 
-  ngOnInit() {
-    if (this.tjommisHub.getConnectionState() == 0) {
-      this.router.navigateByUrl("/login");
-    } 
-  }
 
-  interesser() {
-    this.router.navigateByUrl('/interesser')
-  }
+    ngOnInit() {
+        if (this.tjommisHub.getConnectionState() == 0) {
+            this.router.navigateByUrl("/login");
+        }
+    }
 
-  chat() {
-    this.tjommisHub.Hangout().then(e => {
-      if (e) this.router.navigateByUrl('/loading');
-    }).catch(e => {
-        /* Todo: SnackBar feilmelding til bruker */
-      console.log("Hangout failed: ", e);
-    });
-  }
+    interesser() {
+        this.router.navigateByUrl('/interesser')
+    }
 
-  settings() {
-    this.router.navigateByUrl('/settings');
-  }
+    chat() {
+        this.tjommisHub.Hangout().then(e => {
+            if (e) this.router.navigateByUrl('/loading');
+        }).catch(e => {
+            /* Todo: SnackBar feilmelding til bruker */
+            console.log("Hangout failed: ", e);
+        });
+    }
 
-  animateClick() {
-    anime({
-      targets: 'chatWrapper',
-      translateY: '10vh',
-      duration: 300,
-      direction: 'alternate',
-      easing: 'easeInCubic',
-      scaleX: {
-        value: 1.05,
-        duration: 150,
-        delay: 268
-      },
-      complete: () => {this.chat()}
-    });
-  }
-  
+
+    animateClick() {
+        anime({
+            targets: 'chatWrapper',
+            translateY: '10vh',
+            duration: 300,
+            direction: 'alternate',
+            easing: 'easeInCubic',
+            scaleX: {
+                value: 1.05,
+                duration: 150,
+                delay: 268
+            },
+            complete: () => {
+                this.chat()
+            }
+        });
+    }
+
+
 }
